554ce870c85d2661c26bd0c036ec1e9a0ebefcea
小帅 <2944435683>
2024-11-07 17:21:58 +0800

    no message

--- src/page/recruitment/index.vue
+++ src/page/recruitment/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-image src="https://files.catbox.moe/sik5cg.png" class="test" fit="cover"></el-image>
+  <el-image src="https://files.catbox.moe/sik5cg.png" class="test" fit="cover"></el-image>
   <div class="recruitment">
     <div class="title">
       <span>{{ $t("menu.recruitment") }}</span>
@@ -23,19 +23,17 @@
 import { useI18n } from "vue-i18n";
 const { locale } = useI18n();
 
-const info = ref([
-  {
-    title: {
-      zh: "外贸专员",
-      en: "Foreign Trade Specialist",
-    },
-    number: 1,
-    sub: {
-      zh: `<li>人数：1</li><li>专业要求：化学、化工、国贸等专业优先</li><li>学历要求：本科以上</li><li>年龄：35岁以下</li><p><span style="color: rgb(26, 26, 26); font-size: 14px;"><strong>任职要求</strong></span><span style="color: rgb(26, 26, 26); font-size: 14px;">：<br>1、本科以上学历,英语四级以上，化学、化工、国贸等专业优先；<br>2、1-3 年大宗化学原料贸易经验,对化学品市场情况熟悉；<br>3、良好的人际关系处理能力、沟通能力；<br>4、敏锐的市场洞察能力和勤奋刻苦精神；<br>5、有较高的职业素养,对风险控制及风险分析有深刻认识。<br></span></p><p><span style="color: rgb(26, 26, 26); font-size: 14px;"><strong>工作职责：</strong></span><span style="color: rgb(26, 26, 26); font-size: 14px;"><br>1、处理公司询盘，联系客户，核算报价，合同谈判，成交 订单；<br>2、拓展海外市场，开发新客户， 定期跟踪维护老客户；<br>3、日常处理客户邮件，联系客户，处理订单和售后问题；<br>4、参加国内外展会，拜访客户，市场分析</span></p><p><span style="color: rgb(225, 60, 57);">有意者可将简历发送至邮箱：jc@synova.com.cn</span></p><p><br></p><p><br></p>`,
-      en: "",
-    },
-  },
-]);
+onMounted(() => {
+  getViewStyle();
+});
+
+async function getViewStyle() {
+  const { data } = await $axios.get("/index/getDataApi");
+  let recruitment = JSON.parse(data.recruitment);
+  info.value = recruitment;
+}
+
+const info = ref([]);
 
 const details = ref({
   title: {
@@ -55,7 +53,7 @@ function open(item) {
 </script>
 
 <style lang="scss" scoped>
-.test{
+.test {
   height: 30vh;
   width: 100%;
 }
